0982d4e8efcb6986c8b18fb4542e1ac027f84d7a
SalmanTKhan <salmantkhan@gmail.com>
2017-06-29 14:36:17 -0400

    Update ParseClassGenerator.java
    Fixed createParseGetter not putting field name in quotes for has method.
    Implemented custom printToFile method which also places parseClassName at the top of the file.

--- ClassGen/src/com/taskdesignsinc/tool/ParseClassGenerator.java
+++ ClassGen/src/com/taskdesignsinc/tool/ParseClassGenerator.java
@@ -2,6 +2,10 @@ package com.taskdesignsinc.tool;
 
 import com.taskdesignsinc.util.TextUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Salman T. Khan on 6/29/2017.
  * To be used with Parse Server's Java/Android SDK
@@ -24,6 +28,63 @@ public class ParseClassGenerator extends JavaClassGenerator {
     public void print() {
         System.out.println("@ParseClassName(\"" + mClassName + "\")");
         super.print();
+    }
+
+    public void printToFile(String parentPath) {
+        File classFile;
+        PrintWriter writer;
+        try {
+            if (TextUtils.isEmpty(mPackageName))
+                classFile = new File(parentPath, mClassName + ".java");
+            else {
+                String[] packageNames = mPackageName.split("\\.");
+                StringBuilder builder = new StringBuilder();
+                if (packageNames != null) {
+                    for (String packageName : packageNames) {
+                        //com.taskdesignsinc.android
+                        builder.append(packageName + "/");
+                    }
+                }
+                if (!parentPath.endsWith("/"))
+                    parentPath += "/";
+                classFile = new File(parentPath + builder.toString(), mClassName + ".java");
+            }
+            classFile.getParentFile().mkdirs();
+            if (classFile.exists()) {
+                System.out.println("Class exists");
+                return;
+            }
+            writer = new PrintWriter(classFile);
+            if (!TextUtils.isEmpty(mPackageName))
+                writer.println("package " + mPackageName + ";\n");
+
+            writer.println("@ParseClassName(\"" + mClassName + "\")");
+            writer.print("public class " + mClassName);
+            if (!TextUtils.isEmpty(mParentClass))
+                writer.print(" extends " + mParentClass);
+            if (!mImplementedInterfaces.isEmpty()) {
+                writer.print(" implements ");
+                StringBuilder stringBuilder = new StringBuilder();
+                for (String interfaceName : mImplementedInterfaces) {
+                    stringBuilder.append(interfaceName + ", ");
+                }
+                String interfaces = stringBuilder.toString();
+                interfaces = interfaces.substring(0, interfaces.lastIndexOf(","));
+                writer.print(interfaces);
+            }
+            writer.println(" {\n");
+            for (String s : mArrayFields) {
+                writer.println("\t" + s);
+            }
+            writer.println();
+            for (String s : mArrayAccessors) {
+                writer.println(s);
+            }
+            writer.println("}");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ParseClassGenerator createParseField(String name, ObjectType type) {
@@ -91,7 +152,7 @@ public class ParseClassGenerator extends JavaClassGenerator {
             getString = fieldName.substring(1);
         else
             getString = fieldName;
-        String getParseString = "if (has(\"" + getString.toLowerCase() + "\"))\n\t\treturn get" + TextUtils.capitalizeFirstLetter(objectType) + "(" + getString.toLowerCase() + ");";
+        String getParseString = "if (has(\"" + getString.toLowerCase() + "\"))\n\t\treturn get" + TextUtils.capitalizeFirstLetter(objectType) + "(\"" + getString.toLowerCase() + "\");";
         getString = TextUtils.capitalizeFirstLetter(getString);
         return "\tpublic " + objectType + " get" + getString + "() {\n"
                 + "\t\t" + getParseString + "\n"
